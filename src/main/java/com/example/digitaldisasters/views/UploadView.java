d6ab0ada759186b5ba0bc402abc1623013c5b7b9
Lawrence Lockhart <lawrence@vaadin.com>
2024-09-09 15:54:14 -0700

    polish

--- src/main/java/com/example/digitaldisasters/views/UploadView.java
+++ src/main/java/com/example/digitaldisasters/views/UploadView.java
@@ -42,7 +42,7 @@ public class UploadView extends VerticalLayout {
 
         H1 title = new H1("Digital Disasters");
         title.getStyle().set("font-family", "Roboto, sans-serif");
-        title.getStyle().set("font-size", "3rem");
+        title.getStyle().set("font-size", "3.5rem");
         add(logoLayout, title, getForm());
         addAndExpand(new Scroller(memoriesList));
         updateMemories("");
